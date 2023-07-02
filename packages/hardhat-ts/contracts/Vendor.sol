b9e444a2ee41727f9e9e222c57d1e71fd91c293f
nigsdev <nigsdev@proton.me>
2023-07-02 11:24:13 +0530

    development and unit testing completed

--- packages/hardhat-ts/contracts/Vendor.sol
+++ packages/hardhat-ts/contracts/Vendor.sol
@@ -4,16 +4,37 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import './YourToken.sol';
 
-contract Vendor {
+contract Vendor is Ownable {
   YourToken public yourToken;
+
+  uint256 public constant tokensPerEth = 100;
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event SellTokens(address seller, uint256 amountOfETH, uint256 amountOfTokens);
 
   constructor(address tokenAddress) public {
     yourToken = YourToken(tokenAddress);
   }
 
   // ToDo: create a payable buyTokens() function:
+  function buyTokens() public payable {
+    uint256 amountOfTokens = msg.value * tokensPerEth;
+    yourToken.transfer(msg.sender, amountOfTokens);
+    emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+  function withdraw() public onlyOwner {
+    (bool sent, bytes memory data) = msg.sender.call{value: address(this).balance}("");
+    require(sent, "Failed to send Ether");
+  }
 
   // ToDo: create a sellTokens() function:
+  function sellTokens(uint256 amount) public {
+    yourToken.transferFrom(msg.sender, address(this), amount);
+    uint256 amountOfETH = amount / tokensPerEth;
+    (bool sent, bytes memory data) = msg.sender.call{value: amountOfETH}("");
+    require(sent, "Failed to send Ether");
+    emit SellTokens(msg.sender, amountOfETH, amount);
+  }
+
 }
